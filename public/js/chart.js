e911707d6f175d074a4d7a0fc588b29a22a6e50a
cgroark <colingroark@gmail.com>
2018-02-20 12:38:00 -0800

    rendering chart with carbon data to test functionality

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,28 +1,22 @@
 
-console.log("chaaartdata#####",chartData)
+console.log("chaaartdata#####",chartData.modes)
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: [chartData.modes[0].mode, chartData.modes[1].mode, chartData.modes[2].mode],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Carbon Emissions',
+            data: [chartData.modes[0].carbon, chartData.modes[1].carbon, chartData.modes[2].carbon],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 206, 86, 0.2)'
             ],
             borderColor: [
                 'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 206, 86, 1)'
             ],
             borderWidth: 1
         }]
